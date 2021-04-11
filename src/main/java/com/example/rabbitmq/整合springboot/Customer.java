b542b440f84233b8042c206d0e9c151f2080bbf8
haochenguang123 <haochen_guang@126.com>
2021-04-11 22:49:01 +0800

    springboot与rabbitmq整合

---
+++ src/main/java/com/example/rabbitmq/整合springboot/Customer.java
@@ -0,0 +1,106 @@
+package com.example.rabbitmq.整合springboot;
+
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * TODO 整合springboot
+ *
+ * @author hcg
+ * @version 1.0
+ * @date 2021/4/7 23:04
+ */
+@Component
+public class Customer {
+
+    //第一种模型： hello world  简单队列
+    @RabbitListener(queuesToDeclare = @Queue(value = "hello", declare = "true", autoDelete = "false"))
+    //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void receive1(String message) {
+        System.out.println("message1：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+    //第二种模型： work queue  工作队列  （轮询消费）  --->能者多劳需要做后续配置
+    @RabbitListener(queuesToDeclare = @Queue("work_queue")) //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void workQueue1(String message) throws InterruptedException, IOException {
+        Thread.sleep(2000);
+        System.out.println("message1：" + message + "====时间戳：" + LocalDateTime.now());
+
+    }
+
+    //第二种模型： work queue  工作队列 （轮询消费）
+    @RabbitListener(queuesToDeclare = @Queue("work_queue")) //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void workQueue2(String message) throws IOException {
+        System.out.println("message2：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+    //第三种模型： publish  fanout
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "fanout_exchange", type = "fanout"))})
+    //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void fanout1(String message) throws IOException {
+        System.out.println("message1：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "fanout_exchange", type = "fanout"))})
+    //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void fanout2(String message) throws IOException {
+        System.out.println("message2：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+
+    //第四种： direct routing
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "direct_exchange", type = "direct")
+                    , key = {"direct_queue_info"})})
+    @RabbitHandler
+    public void direct1(String message) throws IOException {
+        System.out.println("message1：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "direct_exchange", type = "direct")
+                    , key = {"direct_queue_error", "direct_queue_info"})
+    })
+    @RabbitHandler
+    public void direct2(String message) throws IOException {
+        System.out.println("message2：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+
+    //第四种： topic routing
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "topic_exchange", type = "topic"),
+                    key = {"topic.*"})
+    })
+    //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void topic1(String message) throws IOException {
+        System.out.println("message1：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue,
+                    exchange = @Exchange(value = "topic_exchange", type = "topic"),
+                    key = {"topic.#"})
+    })
+    //指定某方法作为消息消费的方法，例如监听某 Queue 里面的消息
+    @RabbitHandler
+    public void topic2(String message) throws IOException {
+        System.out.println("message2：" + message + "====时间戳：" + LocalDateTime.now());
+    }
+}
--- src/test/java/com/example/rabbitmq/DemoApplicationTests.java
+++ src/test/java/com/example/rabbitmq/DemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes =DemoApplication.class)
+@SpringBootTest(classes = DemoApplication.class)
 @RunWith(SpringRunner.class)
 public class DemoApplicationTests {
 
@@ -15,10 +15,42 @@ public class DemoApplicationTests {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    //hello world
+    //1.hello world
     @Test
-    public void test() {
-        rabbitTemplate.convertAndSend("hello","hello world");
+    public void helloWorld() {
+        rabbitTemplate.convertAndSend("hello", "hello world");
+    }
+
+    //2.work queue
+    @Test
+    public void workQueue() {
+        for (int i = 0; i < 10; i++) {
+            rabbitTemplate.convertAndSend("work_queue", "hello work queue" + "消息=" + i);
+        }
+    }
+
+    //3.publish --fanout  广播模式
+    //fanout类型的交换机在发送消息的时候，RoutingKey可以不用指定，因为指定了也无效，该交换机会向绑定的所有队列中发送数据
+    @Test
+    public void fanout() {
+        rabbitTemplate.convertAndSend("fanout_exchange", "", "hello fanout queue 消息");
+    }
+
+    //4. routing  --- direct
+    @Test
+    public void routing() {
+        String routing = "direct_queue_error";
+//        String routing = "direct_queue_info";
+        rabbitTemplate.convertAndSend("direct_exchange", routing, "hello direct queue====="+routing);
+    }
+
+
+    //5. topics  ---
+    @Test
+    public void topic() {
+
+        String routing = "topic.info.1";
+        rabbitTemplate.convertAndSend("topic_exchange", routing, "hello topic queue====="+routing);
     }
 
 }
